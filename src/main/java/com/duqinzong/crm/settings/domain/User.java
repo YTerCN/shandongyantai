6bd4f7f9c38cf466d737c8b80a7a70b80092bc88
du <du@duqinzong.com>
2019-05-11 19:47:06 +0800

    123

---
+++ src/main/java/com/duqinzong/crm/settings/domain/User.java
@@ -0,0 +1,23 @@
+package com.duqinzong.crm.settings.domain;
+
+/**
+ * Author  都钦宗
+ */
+public class User {
+
+    private String id ;
+    private String loginAct;
+    private String name;
+    private String loginPwd;
+    private String email;
+    private String expireTime;
+    private String lockState;
+    private String deptno;
+    private String allowIps;
+    private String createTime;
+    private String createBy;
+    private String editTime;
+
+
+
+}
